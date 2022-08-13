ca28211683d92ada9b7ab59bd52f82ca7b950036
srtraj <sharathmadhur@gmail.com>
2022-08-13 13:49:14 +0530

    chatbot folder added

---
+++ lib/chat_page/chat_home_page.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class ChatHome extends StatelessWidget {
+  const ChatHome({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          ///chatList
+          Expanded(
+            child: ListView.builder(
+              itemCount: 5,
+              itemBuilder: (BuildContext context, int index) {
+                return ListTile(
+                    leading: const Icon(Icons.list),
+                    trailing: const Text(
+                      "GFG",
+                      style: TextStyle(color: Colors.green, fontSize: 15),
+                    ),
+                    title: Text("List item $index"));
+              },
+            ),
+          ),
+
+          ///suggestionList
+        ],
+      ),
+    );
+  }
+}
